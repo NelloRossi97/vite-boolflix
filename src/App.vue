280cfcc3e1957387c29b28458dbcd049c538c237
NelloRossi <rossianiello97@gmail.com>
2023-04-19 10:49:41 +0200

    scaffholding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HeaderComponent/>
+  <MainComponent/>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+  export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+      MainComponent
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
