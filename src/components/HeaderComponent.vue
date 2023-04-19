f493ea158528726f127e1c29532ae2cb326b730d
NelloRossi <rossianiello97@gmail.com>
2023-04-19 14:57:54 +0200

    added header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,33 @@
 <template>
-    <div>
-        header
-    </div>
+    <header>
+        <nav class="d-flex justify-content-between align-items-center h-100 px-3">
+            <div class="d-flex align-items-center p-1 h-100">
+                <img src="/image/logo.png" alt="logo">
+                <ul class="nav d-none d-lg-flex">
+                    <li class="nav-item">
+                       <a href="" class="nav-link text-white">Home</a> 
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-white">Serie TV</a> 
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-white">Film</a> 
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-white">Nuovi e popolari</a> 
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-white">Sfoglia per lingua</a> 
+                    </li>
+                </ul>
+            </div>
+            
+            <form action="">
+                <input type="text" placeholder="Cerca film o serie TV" class="me-3">
+                <button type="submit">Cerca</button>
+            </form>
+        </nav>
+    </header>
 </template>
 
 <script>
@@ -11,5 +37,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+header{
+    background: rgb(2,0,36);
+    background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 35%, rgba(20,20,20,1) 100%);
+    height: 60px;
+    width: 100vw;
+    img{
+        width: 15vw;
+        height: 100%;
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import './assets/styles/general.scss'
 import App from './App.vue'
 
-import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
-import '@fortawesome/fontawesome-free/scss/solid.scss';
 
-import 'bootstrap/scss/bootstrap.scss';
 
 createApp(App).mount('#app')
